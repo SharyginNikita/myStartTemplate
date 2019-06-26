b34bfc9cc280f6942537f269432368bd1327a62c
SharyginNikita <sharygin.n@yandex.ru>
2019-06-26 10:48:49 +0300

    add ReadMe, fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,19 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-const browsersync = require('browser-sync');
+const browserSync = require('browser-sync');
 const imagemin = require('gulp-imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-gulp.task('build-html', () => {
+gulp.task('build-html', done => {
     gulp.src('./src/templates/**/*.pug')
         .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(pug())
         .pipe(prettify({}))
         .pipe(gulp.dest('./public'))
-        .pipe(browsersync.stream());
+        .pipe(browserSync.reload({stream: true}))
+    done();
 });
 
 
@@ -29,6 +30,7 @@ gulp.task('build-js', done => {
         .pipe(named())
         .pipe(webpack(require('./webpack.config')))
         .pipe(gulp.dest('./public/js'))
+        .pipe(browserSync.reload({stream: true}))
     done();
 });
 
@@ -41,6 +43,7 @@ gulp.task('build-css', done => {
         .pipe(cleanCSS())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/css'))
+        .pipe(browserSync.reload({stream: true}))
     done();
 });
 
@@ -59,6 +62,7 @@ gulp.task('build-images', done => {
             })
         ]))
         .pipe(gulp.dest('./public/images'))
+        .pipe(browserSync.reload({stream: true}))
     done();
 }); 
 
@@ -66,34 +70,28 @@ gulp.task('build-images', done => {
 gulp.task('build-fonts', done => {
     gulp.src('./src/fonts/**/*')
         .pipe(gulp.dest('./public/fonts'))
+        .pipe(browserSync.reload({stream: true}))
     done();
 });
 
-// gulp.task('browser-sync', done => {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./public"
-//         }
-//     });
-//     done();
-// });
 
-// gulp.task('watch', gulp.parallel('browser-sync', function (done) {
-//     gulp.watch('./src/templates/**/*.pug', gulp.series('build-html'));
-//     done();
-// }));
-//
+gulp.task('serve', () => {
 
-
-gulp.task("serve", () => {
-    browsersync.init({
-        server: "./public/",
-        port: 4000,
-        notify: true
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        },
     });
 
-    gulp.watch('./src/templates/**/*.pug', gulp.parallel("build-html"));
+    gulp.watch('./src/templates/**/*.pug', gulp.parallel('build-html'));    
+    gulp.watch('./src/js/**/*.js', gulp.parallel('build-js'));    
+    gulp.watch('./src/vuex/*', gulp.parallel('build-js'));    
+    gulp.watch('./src/widgets/*', gulp.parallel('build-js'));    
+    gulp.watch('./src/scss/**/*.scss', gulp.parallel('build-css'));    
+    gulp.watch('./src/images/**/*', gulp.parallel('build-images'));
+
 });
+
 gulp.task('default', 
     gulp.parallel(
         'build-fonts',
@@ -104,6 +102,5 @@ gulp.task('default',
         'serve'
     )
 );
-
 
 
